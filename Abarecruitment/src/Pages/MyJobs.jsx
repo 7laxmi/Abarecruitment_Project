86cb30b75098fa70871922936efd6525e0c48fb3
ROHAN-SHENDE <rohanshende1401@gmail.com>
2024-05-11 13:59:33 +0530

    Pagination in My jobs Page

--- Abarecruitment/src/Pages/MyJobs.jsx
+++ Abarecruitment/src/Pages/MyJobs.jsx
@@ -7,14 +7,38 @@ const MyJobs = () => {
     const [searchText, setSearchText] = useState("");
     const[isLoading, setIsLoading] = useState(true);
 
+
+    // Set Current Page
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 4;
+
     useEffect(() => {
         setIsLoading(true);
         fetch("http://localhost:5173/my-job/despicablezeus051@gmail.com")
           .then((res) => res.json())
           .then((data) => {
             setJobs(data);
+            setIsLoading(false)
           });
-      },[]);
+      },[searchText]);
+
+      //Pagination
+      const indexOfLastItem = currentPage * itemsPerPage;
+      const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+      const currentJobs = jobs.slice(indexOfFirstItem, indexOfLastItem)
+
+      // Next Button & Previous btn
+      const nextPage = () => {
+        if(indexOfFirstItem < jobs.length){
+          setCurrentPage(currentPage + 1)
+        }
+      }
+
+      const prevPage = () => {
+        if(currentPage > 1){
+          setCurrentPage(currentPage - 1)
+        }
+      }
 
     const handleSearch = () => {
        const filter = jobs.filter((job) => job.Title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
@@ -25,7 +49,9 @@ const MyJobs = () => {
 
 
     const handleDelete = (id) => {
-      fetch(`http://localhost:5173//job/$(id)`, {})
+      fetch(`http://localhost:5173//job/$(id)`, {
+        method: "Delete"
+      })
       .then(res => res.json)
       .then(data => {
         if(data.acknowledged === true){
@@ -84,9 +110,11 @@ const MyJobs = () => {
           </tr>
         </thead>
 
-        <tbody>
+        {
+          isLoading ? (<div className='flex items-center justify-center h-20'><p>loading.....</p>
+          </div>) : (<tbody>
           {
-            jobs.map((job, index) => (
+            currentJobs.map((job, index) => (
               <tr key={index}>
               <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
                 {index+1}
@@ -110,25 +138,32 @@ const MyJobs = () => {
             ))
           }
   
-        </tbody>
+        </tbody>)
+        }
+
+        
 
       </table>
     </div>
   </div>
 </div>
-<footer className="relative pt-8 pb-6 mt-16">
-  <div className="container mx-auto px-4">
-    <div className="flex flex-wrap items-center md:justify-between justify-center">
-      <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-        <div className="text-sm text-blueGray-500 font-semibold py-1">
-          Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
+      
+   {/* Pagination */}
+        <div className='flex justify-center text-black space-x-8'>
+          {
+            currentPage > 1 && (
+              <button className='hover:underline' onClick={prevPage}>Previous</button>
+            )
+          }
+          {
+            indexOfLastItem < jobs.length && (
+              <button onClick={nextPage} className='hover:underline'>Next</button>
+            )
+          }
         </div>
-      </div>
-    </div>
-  </div>
-</footer>
-</section>
-    </div>
+
+        </section>
+        </div>
   )
 }
 
