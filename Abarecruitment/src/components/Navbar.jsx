2fde7c231c8165128a12f2b6c1dbe46a9c258887
Laxmi Kona <konalaxmi2002@gmail.com>
2024-07-11 15:23:10 +0530

    login

--- Abarecruitment/src/components/Navbar.jsx
+++ Abarecruitment/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import {
+import { 
   Link,
   NavLink
 } from "react-router-dom";
 import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+import Authentication from './Authentication';
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -43,10 +45,8 @@ const Navbar = () => {
             ))
           }
         </ul>
-        <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
-          <Link to="/login" className="py-2 px-5 border rounded">Log in</Link>
-          <Link to="/sign-up" className="py-2 px-5 border rounded bg-blue text-white">Sign up</Link>
-        </div>
+        
+        <Authentication/>
         <div className="md:hidden block">
           <button onClick={handleMenuToggler}>
             {
